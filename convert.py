653804deb3536016461981f735d5be5f8861cdd4
karlchan11 <17688980068@163.com>
2022-04-22 23:08:00 +0800

    use pyqt5 convert simple chinese to traditional chinese

---
+++ convert.py
@@ -0,0 +1,22 @@
+import zhconv
+from shutil import copyfile
+
+"""
+zh-cn 大陆简体
+zh-tw 台灣正體
+zh-hk 香港繁體
+zh-sg 马新简体
+zh-hans 简体
+zh-hant 繁體
+"""
+
+
+def convertToZhtw(inputTsPath, outTsPath):
+    with open(inputTsPath,'r', encoding='UTF-8') as f:
+        content = f.read()
+        with open(outTsPath,'w',encoding='UTF-8') as f1:
+            f1.write(zhconv.convert(content, 'zh-hk'))
+
+#copyfile(r"E:\refactor\hrx\live\palive\uires\soui\translation files\lang_zh-CN.xml", 'lang_zh-CN.xml')
+
+#convertToZhtw('lang_zh-CN.xml', 'hk.xml')
---
+++ main.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import convert
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt5.uic import loadUi 
+
+
+#打包成一个exe时候，文件在临时文件夹的位置
+#pyinstaller -F -w main.py --add-data "ui/convert_lang.ui;ui"  
+
+def resource_path(relative_path):
+    if getattr(sys, 'frozen', False):  #存在frozen 则表示在打包成exe文件运行
+            base_path = sys._MEIPASS
+    else:
+            base_path = os.path.abspath(".") #ide运行
+
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    return os.path.join(base_path, relative_path)
+
+class win(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(300,300)
+      
+        file = 'ui/convert_lang.ui'
+        QMessageBox.information(self, "提示", resource_path(file))
+        loadUi(resource_path(file), self)  #加载UI文件
+
+        self.btnConvert.clicked.connect(self.convert_fun)  #信号槽的连接
+        self.btnsrc.clicked.connect(self.open_file)  
+        self.btndest.clicked.connect(self.save_file)  
+
+    def convert_fun(self):
+        src = self.le_src.text()
+        dst = self.le_dest.text()
+        if src == '' or dst == '':
+            QMessageBox.information(self, "提示", "请选择文件")
+            return
+        if not os.path.exists(src):
+            QMessageBox.information(self, "提示", "源文件不存在")
+            return
+        convert.convertToZhtw(src, dst)
+        QMessageBox.information(self, "提示", "转换完成")
+
+    def open_file(self):
+        fileName,fileType = QFileDialog.getOpenFileName(self, "选取文件", os.getcwd(), 
+        "Text Files(*.xml);;All Files(*)")
+        print(fileName)
+        print(fileType)
+        self.le_src.setText(fileName)
+    def save_file(self):
+        fileName,fileType = QFileDialog.getSaveFileName(self, "选取文件", os.getcwd(), 
+        "Text Files(*.xml);;All Files(*)")
+        print(fileName)
+        print(fileType)
+        self.le_dest.setText(fileName)
+    def exit(self):
+        app = QApplication.instance()
+        app.quit()       
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = win()
+    w.show()
+    sys.exit(app.exec_())
+
